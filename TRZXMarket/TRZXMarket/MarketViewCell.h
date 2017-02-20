e451fd9dc65bc52ef2b64477950dc30404aae3ce
lifp <lifp@mmwipo.com>
2017-02-20 18:17:01 +0800

    编辑.podspec

--- TRZXMarket/TRZXMarket/MarketViewCell.h
+++ TRZXMarket/TRZXMarket/MarketViewCell.h
@@ -5,10 +5,12 @@
 //  Created by N年後 on 2016/12/20.
 //  Copyright © 2016年 Tiancaila. All rights reserved.
 //
-#define kCellIdentifier_MarketViewCell @"MarketViewCell"
 
 #import <UIKit/UIKit.h>
 #import "Project.h"
+
+static NSString *kCellIdentifier_MarketViewCell = @"MarketViewCell";
+
 @interface MarketViewCell : UITableViewCell
 @property (weak, nonatomic) IBOutlet UIImageView *projectCoversImageView;
 @property (strong, nonatomic) Project *project;
@@ -16,6 +18,5 @@
 @property (weak, nonatomic) IBOutlet UILabel *tradeInfoLabel;
 @property (weak, nonatomic) IBOutlet UILabel *contentLabel;
 @property (weak, nonatomic) IBOutlet UIView *bottomView;
-//@property (strong, nonatomic) IBOutlet UILabel *sourceLabel;
 
 @end
